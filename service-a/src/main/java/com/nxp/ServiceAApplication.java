65fc764212b2636f1868bb1bc0779d95513956be
vamsikrishnat93 <vamsit.java8@gmail.com>
2020-01-25 22:49:23 +0530

    add new Annotation

--- service-a/src/main/java/com/nxp/ServiceAApplication.java
+++ service-a/src/main/java/com/nxp/ServiceAApplication.java
@@ -2,8 +2,10 @@ package com.nxp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class ServiceAApplication {
 // added new comments
 	public static void main(String[] args) {
